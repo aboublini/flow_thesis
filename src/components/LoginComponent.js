052c789c58be6a31342411d8a4aa06e84d80be3f
aboublini <p19117@unipi.gr>
2023-01-26 13:33:20 +0200

    Login DONE (only username & password check)

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -1,72 +1,69 @@
-//import React, {useState} from 'react';
 import './LoginComponentStyle.css';
 import './VideoStyle.css'
 import colourVideo from "../visual-material/LoginBackround.mp4";
+import Logo from '../visual-material/logo.png';
 //import {Link} from "react-router-dom";
 
 const LoginComponent = () => {
-    // const [errorMessages, setErrorMessages] = useState({});
-    // const [isSubmitted, setIsSubmitted] = useState(false);
-    //
-    // // User Login info
-    // const database = [
-    //     {
-    //         username: "aboublini",
-    //         password: "pass"
-    //     },
-    //     {
-    //         username: "user",
-    //         password: "pass2"
-    //     }
-    // ];
-    //
-    // const error = {
-    //     incorrectCredentials: "The credentials you provided are invalid.",
-    // };
-    //
-    // const handleLogin = (event) => {
-    //     //Prevent page reload
-    //     event.preventDefault();
-    //
-    //     const {uname, pass} = document.forms[0];
-    //
-    //     // Find user login info
-    //     const userData = database.find((user) => user.username === uname.value);
-    //
-    //     // Compare user info
-    //     if (userData) {
-    //         if ((userData.password !== pass.value) || (userData.username !== uname.value)) {
-    //             // Invalid password
-    //             setErrorMessages({ name: "incorrectCredentials", message: error.incorrectCredentials });
-    //         } else {
-    //             setIsSubmitted(true);
-    //         }
-    //     }
-    // };
-    //
-    // const renderErrorMessage = (name) =>
-    //     name === errorMessages.name && (
-    //         <div className="error">{errorMessages.message}</div>
-    //     );
+    const handleLogin = user => {
 
+        user.preventDefault();
+
+        // Check if user info is correct
+        if (user.target.username.value === "aboublini" && user.target.password.value === "password") {
+            alert("Successful login!");
+            // Clear inputs
+            user.target.username.value = "";
+            user.target.password.value = "";
+        } else {
+            alert("The credentials you provided are invalid. Please try again.");
+            // Clear inputs
+            user.target.username.value = "";
+            user.target.password.value = "";
+        }
+    }
+
+    const ResetPassword = () => {
+        let message = prompt("Please enter your registered mail:", "yourmail@example.gr");
+
+        // If inserts a mail something and presses okay
+        if (message !== "" && message != null && message !== "yourmail@example.gr") {
+
+            // If the provided email address is correct
+            alert("An email has been sent to " + message + " with instructions on how to recover your password.");
+
+        } else if (message == null) { // If user presses cancel
+
+            // Do nothing
+
+        } else {
+            // If the provided email address is wrong
+            alert("Please insert a valid email address or press Cancel.");
+            ResetPassword();
+
+        }
+    }
+
+    // Render the login form
     return (
         <div className="hero">
             <video autoPlay loop muted id="video">
                 <source src={colourVideo} type="video/mp4"/>
             </video>
             <div className="content">
-                <form>
-                    <h1>Login</h1>
+                <form onSubmit={handleLogin}>
+                    <img src={Logo} className="img" alt=""/>
                     <label>Username</label>
-                    <input type="text" name="uname" required></input>
+                    <input type="text" name="username" required/>
                     <label>Password</label>
-                    <input type="password" name="pass" required></input>
+                    <input type="password" name="password" required/>
                     <button className="btn">Login</button>
-                    <p>Don't have an account? Sign up</p>
+                    <p>Forgot your password? <u onClick={ResetPassword}>Reset my password</u></p>
                 </form>
             </div>
         </div>
     );
+
 }
 
 export default LoginComponent;
